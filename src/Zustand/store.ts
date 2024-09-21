1de5d8cd7bc67c3aa6ad8f394c8bf3f0109ab886
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-09-21 20:07:24 +0200

    added final styles

--- src/Zustand/store.ts
+++ src/Zustand/store.ts
@@ -3,11 +3,6 @@ import { create } from 'zustand';
 import { Game } from '../types/GameData'; // Импортируйте интерфейс Game
 
 
-axios.defaults.baseURL = 'https://getboardgames-jxxjux7fua-ey.a.run.app';
-
-
-const UNSPLASH_ACCESS_KEY = 'GP84ettFgJ1FFpSDsLlX-G4i_h-DhP_6pjxzYXWHr48';
-
 interface GameState {
   games: Game[];
   filter: string; 
@@ -27,17 +22,12 @@ const useGameStore = create<GameStore>((set) => ({
   error: null,
 
   fetchGames: async () => {
-    set({ loading: true, error: null });
+    set({ loading: true, error: null }); 
     try {
       const response = await axios.get('/'); 
-      const gamesWithImages = await Promise.all(response.data.map(async (game: Game) => {
-        const imageUrl = await fetchImageForGame(`${game.name} boardgame`);
-        return { ...game, image: imageUrl }; 
-      }));
-      set({ games: gamesWithImages, loading: false });
+      set({ games: response.data, loading: false }); 
     } catch (error) {
-      console.log(error);
-      set({ loading: false, error: 'Failed to fetch games.' });
+      set({ loading: false, error: `${error}Failed to fetch games.` }); 
     }
   },
 
@@ -49,28 +39,5 @@ const useGameStore = create<GameStore>((set) => ({
   },
 }));
 
-const fetchImageForGame = async (gameName: string) => {
-    const query = `${gameName} board game box`;
-    
-    try {
-      const response = await axios.get('https://api.unsplash.com/search/photos', {
-        params: {
-          query: query,
-          per_page: 1, 
-        },
-        headers: {
-          Authorization: `Client-ID ${UNSPLASH_ACCESS_KEY}`,
-        },
-      });
-  
-      if (response.data.results && response.data.results.length > 0) {
-        return response.data.results[0].urls.regular; 
-      }
-    } catch (error) {
-      console.error('Error fetching image:', error);
-    }
-  
-    return 'https://example.com/default-image.jpg'; 
-  };
 
 export default useGameStore;
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 import useGameStore from "../Zustand/store";
 import { Game } from "../types/GameData";
 import {
+  BsEmojiGrin,
+  BsEmojiHeartEyes,
+  BsEmojiLaughing,
+  BsEmojiWink,
   BsSortAlphaDown,
   BsSortAlphaDownAlt,
   BsSortNumericDown,
@@ -14,6 +18,8 @@ const GameTable = () => {
   const [sortField, setSortField] = useState("");
   const [sortOrder, setSortOrder] = useState("asc");
   const [filteredGames, setFilteredGames] = useState(games);
+
+  const emojiIcons = [<BsEmojiGrin />, <BsEmojiHeartEyes />, <BsEmojiLaughing />, <BsEmojiWink />];
 
   useEffect(() => {
     fetchGames();
@@ -62,111 +68,108 @@ const GameTable = () => {
         placeholder="Search games..."
         value={filter}
         onChange={(e) => setFilter(e.target.value)}
-        className="mb-4 p-2 rounded text-black"
+        className="mb-4 p-2  text-black outline-[#18910d] bg-[#d7eed5] "
       />
 
       {filteredGames.length > 0 ? (
-   <table className="min-w-full table-auto border-collapse border border-gray-800">
+        <div className="min-w-full rounded-[8px] border-[1px] border-#E4E4EF text-black ">
+          <ul className="flex w-[100%]">
+            <li
+              onClick={() => handleSort("name")}
+              className="px-4 py-2 cursor-pointer h-[50px] flex justify-between items-center w-[100%] font-extrabold"
+            >
+              <span>Name</span>
+              {sortField === "name" && sortOrder === "asc" ? (
+                <BsSortAlphaDown size={30} />
+              ) : sortField === "name" && sortOrder === "desc" ? (
+                <BsSortAlphaDownAlt size={30} />
+              ) : (
+                <BsSortAlphaDown size={30} />
+              )}
+            </li>
+            <li
+              onClick={() => handleSort("releaseYear")}
+              className="px-4 py-2 cursor-pointer h-[50px] flex justify-between items-center w-[100%] font-extrabold"
+            >
+              <span>Release Year</span>
+              {sortField === "releaseYear" && sortOrder === "asc" ? (
+                <BsSortNumericDown size={30} />
+              ) : sortField === "releaseYear" && sortOrder === "desc" ? (
+                <BsSortNumericDownAlt size={30} />
+              ) : (
+                <BsSortNumericDown size={30} />
+              )}
+            </li>
+            <li
+              onClick={() => handleSort("players")}
+              className="px-4 py-2 cursor-pointer h-[50px] flex justify-between items-center w-[100%] font-extrabold"
+            >
+              <span>Players</span>
+              {sortField === "players" && sortOrder === "asc" ? (
+                <BsSortNumericDown size={30} />
+              ) : sortField === "players" && sortOrder === "desc" ? (
+                <BsSortNumericDownAlt size={30} />
+              ) : (
+                <BsSortNumericDown size={30} />
+              )}
+            </li>
+            <li
+              onClick={() => handleSort("publisher")}
+              className="px-4 py-2 cursor-pointer h-[50px] flex justify-between items-centerl w-[100%] font-extrabold"
+            >
+              <span>Publisher</span>
+              {sortField === "publisher" && sortOrder === "asc" ? (
+                <BsSortAlphaDown size={30} />
+              ) : sortField === "publisher" && sortOrder === "desc" ? (
+                <BsSortAlphaDownAlt size={30} />
+              ) : (
+                <BsSortAlphaDown size={30} />
+              )}
+            </li>
+            <li
+              onClick={() => handleSort("type")}
+              className="px-4 py-2 cursor-pointer h-[50px] flex justify-between items-center w-[100%] font-extrabold"
+            >
+              <span>Type</span>
+              {sortField === "type" && sortOrder === "asc" ? (
+                <BsSortAlphaDown size={30} />
+              ) : sortField === "type" && sortOrder === "desc" ? (
+                <BsSortAlphaDownAlt size={30} />
+              ) : (
+                <BsSortAlphaDown size={30} />
+              )}
+            </li>
+          </ul>
 
-  <thead>
-    <tr>
-      <th
-        onClick={() => handleSort("name")}
-        className="border px-4 py-2 cursor-pointer h-[100px] text-left"
-      >
-        <div className="flex justify-between items-center">
-          <span>Name</span>
-          {sortField === "name" && sortOrder === "asc" ? (
-            <BsSortAlphaDown size={30} />
-          ) : sortField === "name" && sortOrder === "desc" ? (
-            <BsSortAlphaDownAlt size={30} />
-          ) : (
-            <BsSortAlphaDown size={30} />
-          )}
+          <ul className="w-full">
+  {filteredGames.map((game, index) => (
+    <li
+      key={game.id}
+      className={`flex ${index % 2 === 0 ? "bg-[#d7eed5]" : "bg-white"} rounded-[8px] mb-2`}
+    >
+      <span className="px-4 py-2 h-[50px] flex justify-between items-center w-[100%] rounded-[8px] border-[1px] border-gray-300">
+        {game.name || "no data"}
+        {/* Render a random emoji icon */}
+        <div className="ml-2">
+          {emojiIcons[Math.floor(Math.random() * emojiIcons.length)]}
         </div>
-      </th>
-      <th
-        onClick={() => handleSort("releaseYear")}
-        className="border px-4 py-2 cursor-pointer h-[100px] text-left"
-      >
-        <div className="flex justify-between items-center">
-          <span>Release Year</span>
-          {sortField === "releaseYear" && sortOrder === "asc" ? (
-            <BsSortNumericDown size={30} />
-          ) : sortField === "releaseYear" && sortOrder === "desc" ? (
-            <BsSortNumericDownAlt size={30} />
-          ) : (
-            <BsSortNumericDown size={30} />
-          )}
+      </span>
+      <span className="px-4 py-2 h-[50px] w-[100%] rounded-[8px] border-[1px] border-gray-300">
+        {game.releaseYear || "no data"}
+      </span>
+      <span className="px-4 py-2 h-[50px] w-[100%] rounded-[8px] border-[1px] border-gray-300">
+        {game.players.min || "no data"} - {game.players.max || "no data"}
+      </span>
+      <span className="px-4 py-2 h-[50px] w-[100%] rounded-[8px] border-[1px] border-gray-300">
+        {game.publisher || "no data"}
+      </span>
+      <span className="px-4 py-2  h-[50px] w-[100%] rounded-[8px] border-[1px] border-gray-300">
+        {game.type || "no data"}
+      </span>
+    </li>
+  ))}
+</ul>
         </div>
-      </th>
-      <th
-        onClick={() => handleSort("players")}
-        className="border px-4 py-2 cursor-pointer h-[100px] text-left"
-      >
-        <div className="flex justify-between items-center">
-          <span>Players</span>
-          {sortField === "players" && sortOrder === "asc" ? (
-            <BsSortNumericDown size={30} />
-          ) : sortField === "players" && sortOrder === "desc" ? (
-            <BsSortNumericDownAlt size={30} />
-          ) : (
-            <BsSortNumericDown size={30} />
-          )}
-        </div>
-      </th>
-      <th
-        onClick={() => handleSort("publisher")}
-        className="border px-4 py-2 cursor-pointer h-[100px] text-left"
-      >
-        <div className="flex justify-between items-center">
-          <span>Publisher</span>
-          {sortField === "publisher" && sortOrder === "asc" ? (
-            <BsSortAlphaDown size={30} />
-          ) : sortField === "publisher" && sortOrder === "desc" ? (
-            <BsSortAlphaDownAlt size={30} />
-          ) : (
-            <BsSortAlphaDown size={30} />
-          )}
-        </div>
-      </th>
-      <th
-        onClick={() => handleSort("type")}
-        className="border px-4 py-2 cursor-pointer h-[100px] text-left"
-      >
-        <div className="flex justify-between items-center">
-          <span>Type</span>
-          {sortField === "type" && sortOrder === "asc" ? (
-            <BsSortAlphaDown size={30} />
-          ) : sortField === "type" && sortOrder === "desc" ? (
-            <BsSortAlphaDownAlt size={30} />
-          ) : (
-            <BsSortAlphaDown size={30} />
-          )}
-          
-        </div>
-      </th>
-    </tr>
-  </thead>
-          <tbody>
-            {filteredGames.map((game) => (
-              <tr key={game.id}>
-                <td className="border px-4 py-2">{game.name || "no data"}</td>
-                <td className="border px-4 py-2">
-                  {game.releaseYear || "no data"}
-                </td>
-                <td className="border px-4 py-2">
-                  {game.players.min || "no data"} -{" "}
-                  {game.players.max || "no data"}
-                </td>
-                <td className="border px-4 py-2">
-                  {game.publisher || "no data"}
-                </td>
-                <td className="border px-4 py-2">{game.type || "no data"}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
       ) : (
         !loading && <p>No games available</p>
       )}
