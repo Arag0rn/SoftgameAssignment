d5eebf90a591a40347e7011bab0ff929922b0c42
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-09-21 11:13:37 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from 'tailwindcss'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/SoftgameAssignment/',
   css: {
     postcss: {
       plugins: [
